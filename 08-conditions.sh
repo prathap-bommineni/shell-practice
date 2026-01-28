8ec2d01b49aaa8dbac00ac39ef30c7d84af91487
prathap <pratapbch@gmail.com>
2026-01-28 17:29:43 +0530

    action

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#exmaple varible 
-num =12
 
-#if condition 
+# Example variable
+num=10
 
-if [$num -gt 6]; then
-echo "Number is greater then 5"
-else 
-echo "Number is 5 or less"
+# If condition
+if [ $num -gt 5 ]; then
+    echo "Number is greater than 5"
+else
+    echo "Number is 5 or less"
 fi
