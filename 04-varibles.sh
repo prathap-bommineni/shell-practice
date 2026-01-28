6ffae1715f27d46a4bec5faac34ce9fe72a2db0b
prathap <pratapbch@gmail.com>
2026-01-28 16:28:14 +0530

    action

---
+++ 04-varibles.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:: How are yuo?"
+echo "$PERSON2:: Hi Suresh, I am fine thanks for asking. How are you doing?"
+echo "$PERSON1:: doing fine. What are you learning Ramesh?"
+echo "$PERSON2:: I am learning Shell. What about you?"
